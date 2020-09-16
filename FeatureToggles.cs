73a5164101e8db867844e57bf95280610349c9b7
jenrybornales <55679062+jenrybornales@users.noreply.github.com>
2020-09-16 10:52:45 +0800

    Add project files.

---
+++ FeatureToggles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETMVCORE2
+{
+
+
+    public class FeatureToggles
+    {
+        public bool DeveloperExceptions { get; set; }
+    }
+}
